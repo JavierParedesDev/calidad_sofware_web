f79d4122b71b9cb83a79e5899e583cdcb18e8104
BenjaminAnabalon <be.anabalon@duocuc.cl>
2024-09-03 11:08:05 -0400

    pagina productos

--- login.js
+++ login.js
@@ -3,31 +3,44 @@ document.addEventListener('DOMContentLoaded', () => {
     const errorMessage = document.getElementById('error-message');
 
     form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Evita el envío del formulario para validar primero
+        event.preventDefault();
 
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
 
         // Validación simple
         if (username === '' || password === '') {
-            errorMessage.textContent = 'Por favor, complete todos los campos.';
-            return;
+            Swal.fire({
+                title: "Por favor, ingrese sus datos correctamente",
+                icon: "error", 
+                confirmButtonText: 'OK'
+            });
+            return; // Salir de la función si hay un error
         }
 
-        // Validaciones adicionales (puedes personalizar según sea necesario)
         if (username.length < 5) {
-            errorMessage.textContent = 'El nombre de usuario debe tener al menos 5 caracteres.';
-            return;
+            Swal.fire({
+                title: "Por favor, ingrese sus datos correctamente",
+                icon: "error", 
+                confirmButtonText: 'OK'
+            });
+            
         }
 
         if (password.length < 8) {
-            errorMessage.textContent = 'La contraseña debe tener al menos 8 caracteres.';
-            return;
+            Swal.fire({
+                title: "Por favor, ingrese sus datos correctamente",
+                icon: "error", 
+                confirmButtonText: 'OK'
+            });
         }
 
-        // Si todo está bien, puedes continuar con el proceso de login
-        errorMessage.textContent = ''; // Limpiar mensaje de error
-        alert('¡Inicio de sesión exitoso!'); // Aquí podrías redirigir a otra página o enviar la información al servidor
-        // form.submit(); // Si deseas enviar el formulario después de la validación
+        errorMessage.textContent = ''; 
+        Swal.fire({
+            title: '¡Inicio de sesión exitoso!',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+       
     });
 });
